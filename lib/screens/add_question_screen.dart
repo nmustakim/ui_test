e8cdf8f248f21de8ca166cd550a72998d517e0e3
Nayeem Mustakim <nmustakim98@gmail.com>
2023-12-15 23:46:04 +0600

    list util

--- lib/screens/add_question_screen.dart
+++ lib/screens/add_question_screen.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:ui_test/models/add_question_card_model.dart';
+
 import 'package:ui_test/widgets/add_question_card.dart';
 
-List<AddQuestionCardModel> questionCardItemList = [
-  AddQuestionCardModel('Multiple Choice', 'assets/images/GitFork-r.png'),
-  AddQuestionCardModel('True or False', 'assets/images/PlusMinus-r.png'),
-  AddQuestionCardModel('Puzzle', 'assets/images/DiamondsFour-r.png'),
-  AddQuestionCardModel('Photo Choice', 'assets/images/Camera-r.png'),
-  AddQuestionCardModel('Checkbox', 'assets/images/CheckSquare-r.png'),
-  AddQuestionCardModel('Open ended', 'assets/images/SlidersHorizontal-r.png'),
-  AddQuestionCardModel('Poll', 'assets/images/ChartBar-r.png'),
-  AddQuestionCardModel(
-      'Fill-in-the-Blank', 'assets/images/TextAlignCenter-r.png'),
-  AddQuestionCardModel('Flash Card', 'assets/images/Cards-r.png'),
-];
+import '../utils/lists.dart';
+
 
 class AddQuestionScreen extends StatelessWidget {
   const AddQuestionScreen({super.key});
@@ -49,9 +39,9 @@ class AddQuestionScreen extends StatelessWidget {
                 childAspectRatio: 183 / 121,
                 crossAxisSpacing: 6,
                 mainAxisSpacing: 6),
-            itemCount: questionCardItemList.length,
+            itemCount: Lists().questionCardItemList.length,
             itemBuilder: (context, index) {
-              final cardItem = questionCardItemList[index];
+              final cardItem = Lists().questionCardItemList[index];
               return AddQuestionCard(
                   title: cardItem.title, icon: cardItem.icon);
             }),
---
+++ lib/utils/lists.dart
@@ -0,0 +1,16 @@
+import '../models/add_question_card_model.dart';
+
+class Lists {
+  List<AddQuestionCardModel> questionCardItemList = [
+    AddQuestionCardModel('Multiple Choice', 'assets/images/GitFork-r.png'),
+    AddQuestionCardModel('True or False', 'assets/images/PlusMinus-r.png'),
+    AddQuestionCardModel('Puzzle', 'assets/images/DiamondsFour-r.png'),
+    AddQuestionCardModel('Photo Choice', 'assets/images/Camera-r.png'),
+    AddQuestionCardModel('Checkbox', 'assets/images/CheckSquare-r.png'),
+    AddQuestionCardModel('Open ended', 'assets/images/SlidersHorizontal-r.png'),
+    AddQuestionCardModel('Poll', 'assets/images/ChartBar-r.png'),
+    AddQuestionCardModel(
+        'Fill-in-the-Blank', 'assets/images/TextAlignCenter-r.png'),
+    AddQuestionCardModel('Flash Card', 'assets/images/Cards-r.png'),
+  ];
+}
